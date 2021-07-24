791a50e34e9ddc0995ebeb6253fe8290d3329474
BeataMaro <beatamaro@interia.pl>
2021-07-24 16:52:57 +0200

    Seo component

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,74 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-sass`],
+  siteMetadata: {
+    title: `SelfMAX`,
+    description:
+      "Zadaszenia tarasowe, carporty, pergole, ogrody zimowe. Szybka realizacja, 10-letnia gwarancja, trwałość i estetyka.",
+    copyright: "U+00A9 2021 | Beata Maro",
+    contact: "beata.maro@yahoo.com",
+    author: "Beata Maro",
+    menuLinks: [
+      {
+        name: `Strona główna`,
+        link: `/`,
+      },
+      {
+        name: `Produkty`,
+        link: `/products`,
+        submenu: [
+          {
+            name: `Zadaszenia z poliweglanu`,
+            link: `/poliweglan`,
+          },
+          {
+            name: `Carporty`,
+            link: `/carporty`,
+          },
+          {
+            name: `Dachy rzymskie`,
+            link: `/dachyrzymskie`,
+          },
+          {
+            name: `Lamele`,
+            link: `/lamele`,
+          },
+          {
+            name: `Ogrody zimowe`,
+            link: `/ogrodyzimowe`,
+          },
+          {
+            name: `Zabudowa`,
+            link: `/zabudowa`,
+          },
+        ],
+      },
+      {
+        name: `O nas`,
+        link: `/about`,
+        submenu: [],
+      },
+
+      {
+        name: `Kontakt`,
+        link: `/contact`,
+        submenu: [],
+      },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+  ],
 }
---
+++ src/pages/contact.js
@@ -0,0 +1,63 @@
+import React from "react"
+// import React, { useEffect, useRef } from "react"
+// import ContactForm from "../components/ContactForm/ContactForm"
+import styled from "styled-components"
+// import { FormspreeProvider } from "@formspree/react"
+// import { animationMoveY } from "../styles/Animation"
+
+// import ContactInfo from "../components/ContactInfo"
+// import Map from "../components/Map"
+import Seo from "../components/Seo"
+
+const StyledContactPage = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+
+  article {
+    @media (max-width: 768px) {
+      grid-column: 1 / -1;
+    }
+
+    &:nth-of-type(1) {
+      grid-column: 1 / 3;
+    }
+    &:nth-of-type(2) {
+      display: grid;
+      place-items: center;
+      grid-column: 3 / 5;
+    }
+    &:nth-of-type(3) {
+      grid-column: 1 / -1;
+    }
+  }
+`
+
+const Contact = () => {
+  //   const sectionRef = useRef(null)
+  //   useEffect(() => {
+  //     animationMoveY(sectionRef.current, true)
+  //   }, [])
+
+  return (
+    <>
+      <Seo title="Kontakt" />
+      <section>
+        <h2 className="section-title">Kontakt</h2>
+        <StyledContactPage>
+          <article>
+            {/* <FormspreeProvider project="1723153946022771743">
+              <ContactForm />
+            </FormspreeProvider> */}
+          </article>
+          <article></article>
+          <article></article>
+          {/* <ContactInfo /> */}
+          {/* <Map /> */}
+        </StyledContactPage>
+      </section>
+    </>
+  )
+}
+
+export default Contact
