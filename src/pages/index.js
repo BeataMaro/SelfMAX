b63b132d45b4ed8e61a794e3c8a3f10880b98405
BeataMaro <beatamaro@interia.pl>
2021-07-24 17:07:11 +0200

    gatsby-plugin-image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
   return (
     <div>
-      <p>Kicior</p>Hello world!
+      <p>Kicior</p>
+      <StaticImage
+        src="../assets/hero/jonas-jaeken-yellow-car-unsplash-cropp.jpg"
+        alt="Yellow car parked in the driveway in front of the modern house"
+        placeholder="blurred"
+      />
     </div>
   )
 }
