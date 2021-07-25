c9536a82eec3cdb09aa4c109de10f576fa822c00
BeataMaro <beatamaro@interia.pl>
2021-07-25 17:21:50 +0200

    Kwes form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -15,12 +15,14 @@ const ContactForm = () => {
         <label htmlFor="name">Imię</label>
         <input type="text" name="name" rules="max:255" />
         <label htmlFor="email">Adres e-mail</label>
-        <input type="email" name="email_address" />
+        <input type="email" name="email" rules="required|email" />
         <label htmlFor="phone">Numer telefonu</label>
-        <input name="phone" />
+        <input type="text" name="phone" rules="numeric" />
+        <label htmlFor="message">Wiadomość</label>
+        <textarea name="message" rules="max: 500" />
         <button type="submit">Wyślij</button>
 
-        <input type="checkbox" name="terms" />
+        <input type="checkbox" name="terms" rules="accepted" />
         <label htmlFor="terms">
           Zapoznałem/łam się z informacją o administratorze i przetwarzaniu
           danych.
